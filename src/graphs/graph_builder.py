c8434a408781a7cf9991cb81ae071355270bc5d0
Rohit Priyesh Jagtap <jagtaprohit005011999@gmail.com>
2025-07-15 22:31:24 +0530

    Implement blog generation endpoint and setup graph builder; add configuration files

--- src/graphs/graph_builder.py
+++ src/graphs/graph_builder.py
@@ -23,3 +23,18 @@ class GraphBuilder:
         self.graph.add_edge("content_generation",END)
 
         return self.graph
+    
+    def setup_graph(self,usecase):
+        if usecase=="topic":
+            self.build_topic_graph()
+            
+        return self.graph.compile()
+    
+
+## Below code is for the langsmith langgraph studio
+llm =GroqLLM().get_llm()
+
+# Get the graph
+graph_builder = GraphBuilder(llm)
+graph=graph_builder.build_topic_graph().compile()
+
